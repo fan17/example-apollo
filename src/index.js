d4012e5a31198863c9f63b06f5128ac38f750498
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2018-07-17 20:06:03 +0200

    sample query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { ExchangeRatesIndex } from './ExchangeRates/ExchangeRatesIndex.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRatesIndex />
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"));
+
 registerServiceWorker();

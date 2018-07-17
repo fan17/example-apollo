c60abae5fe753204c2da8f9bc591c561f75c50d5
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2018-07-17 20:38:03 +0200

    additional routes, to check cache

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import { render } from "react-dom";
 import registerServiceWorker from './registerServiceWorker';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import { ExchangeRatesIndex } from './ExchangeRates/ExchangeRatesIndex.js';
+import Dashboard from './Dashboard.js';
 
 const client = new ApolloClient({
   uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
 });
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <ExchangeRatesIndex />
-    </div>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <div>
+                <h2>My first Apollo app 🚀</h2>
+                <nav>
+                    <Link to="/">Dashboard</Link>
+                    &nbsp;|&nbsp;
+                    <Link to="/exchange-rates">ExchangeRate</Link>
+                </nav>
+                <div>
+                    <Route exact path="/" component={Dashboard}/>
+                    <Route path="/exchange-rates" component={ExchangeRatesIndex}/>  
+                </div>
+            </div>
+        </BrowserRouter>
   </ApolloProvider>
 );
 
